cf4495eae48518582e272130f51f933d8f8564fc
Nicholas Vuono <nicholas.vuono@gmail.com>
2023-03-22 19:57:41 -0400

    fix semantic def, lexer

---
+++ lexer.js
@@ -0,0 +1,14 @@
+import moo from 'moo'
+
+module.exports = moo.compile({
+    whitespace:      /[ \t]+/,
+    number:          { match: /0|[1-9][0-9]*/, value: Number },
+    string:          /"(?:\\["\\]|[^\n"\\])*"/,
+    left_bracket:    '[',
+    right_bracket:   ']',
+    left_paren:      '(',
+    right_paren:     ')',
+    assignment_op:   "=",
+    identifier:      /[a-zA-Z_][a-zA-Z0-9_]*/,
+    newline:         { match: /\n/, lineBreaks: true },
+});
